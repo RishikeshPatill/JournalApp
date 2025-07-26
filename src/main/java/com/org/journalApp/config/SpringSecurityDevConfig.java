85ab338fb0b46ae917491c8c6f2ec78d029b35ff
RishikeshPatill <patilrishi410@gmail.com>
2025-07-26 15:12:56 +0530

    video 26 Spring Boot profiles done on code as well and understood and the app is running error free working fine now moving on the video 27 and then we will be left with only 20 videos we are half way there, now today will do 4 more to make it 30 then by the end of this month i have to finish it as 5 days left each day 4 video then it will be completed along with each video notes written in the yml okay

--- src/main/java/com/org/journalApp/config/SpringSecurityDevConfig.java
+++ src/main/java/com/org/journalApp/config/SpringSecurityDevConfig.java
@@ -4,6 +4,7 @@ import com.org.journalApp.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,21 +14,26 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+//@Profile("dev") // --> just for example
+public class SpringSecurityDevConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserDetailsServiceImpl userDetailsService;
 
+    //csrf token should always be enabled ,csrf tokens are embedded on form so
+    //that it does not get stored in the cookies so no worries.
+    //by default cross-site-request-forgery is enabled which expects that with our request
+    //we will send a token which we didn't so disabled, if there is no session then we can disable csrf.
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/journal/**", "/user/**").authenticated()   // bro u can see here in this commit i did the change u asked for
-                .antMatchers("/admin/**").hasRole("ADMIN")             // bro u can see here in this commit i did the change u asked for
+                .antMatchers("/journal/**", "/user/**").authenticated()
+                .antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().permitAll()
                 .and()
                 .httpBasic();
-        http.csrf().disable();                                           //csrf token should always be enabled , csrf tokens are embedded on form so that it does not get stored in the cookies so no worries.
-                                                                        //by default cross-site-request-forgery is enabled which expects that with our request we will send a token which we didn't so disabled, if there is no session then we can disable csrf.
+        http.csrf().disable();
     }
 
     @Override
