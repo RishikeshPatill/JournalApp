1bd34a4cd0821b0ca8066e0995ed9725d29b41fd
RishikeshPatill <patilrishi410@gmail.com>
2025-08-05 15:27:57 +0530

    redirect to health check

---
+++ src/main/java/com/org/journalApp/controller/RedirectController.java
@@ -0,0 +1,13 @@
+package com.org.journalApp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RedirectController {
+
+    @GetMapping("/")
+    public String redirectToHealthCheck() {
+        return "redirect:/journal_app/health-check";
+    }
+}
